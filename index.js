55b2025599e153395e6421fafac114cfb6ce614c
Martin Thorsen Ranang <mtr@ranang.org>
2016-11-09 13:45:06 +0100

    Added crawlers to guess the location of, and retrieve, license texts.

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ var chalk = require('chalk');
 var fs = require('fs');
 var path = require('path');
 var program = require('commander');
+var Promise = require('promise');
 var npmLicenseCrawler = require('npm-license-crawler');
+var request = require('request');
 
 var version = '1.0.0';
 
@@ -35,37 +37,189 @@ function isPackageDependency(name, dependency, packageJson) {
         dependency.parents === packageJson.name;
 }
 
-function parseCrawlerResults(packageJson, licences) {
-    var dependencies = _.transform(licences,
-        function _dependencyTransformer(result, value, key) {
-            var parts = key.split('@'),
-                version = parts.pop(),
-                name = parts.join('@');
+var _ignoreGitHttpPrefixRegEx = /^git\+(https?.*)$/;
+var _ignoreGitSshPrefixRegEx = /^git\+ssh:\/\/([^@]+)@?(.*)$/;
+var _replaceGitHubTypoRegEx = /^https:\/\/wwwhub\.com\/(.*)$/;
 
-            if (!program.strict || isPackageDependency(name, value, packageJson)) {
-                result.push({
-                    name: name,
-                    version: version,
-                    licenses: value.licenses,
-                    repository: value.repository,
-                    licenseUrl: value.licenseUrl,
-                    parents: value.parents
-                });
+function _cleanupUrls(str) {
+    if (typeof str === 'string') {
+        return str.replace(_ignoreGitHttpPrefixRegEx, '$1')
+            .replace(_ignoreGitSshPrefixRegEx, 'https://$2')
+            .replace(_replaceGitHubTypoRegEx, 'https://github.com/$1');
+    }
+    return str;
+}
+
+var _licenseGuesses = [
+    'LICENSE',
+    'LICENSE.txt',
+    'LICENSE.md'
+];
+
+var createHypothesisCrawler = function (guessUrl, i, crawlers) {
+    return new Promise(function (resolve, reject) {
+
+        var crawler = request.head(guessUrl,
+            function _handleResponse(error, response, body) {
+                if (error) {
+                    consoleLog(chalk.red('HTTP request error:', error));
+                    resolve({
+                        isValid: false,
+                        url: guessUrl,
+                        reason: 'HTTP request error: ' + error
+                    });
+                    return;
+                }
+
+                if (response) {
+                    /*
+                     console.log('guessUrl', guessUrl);
+                     console.log('response', response.request.href);
+                     console.log('response.statusCode', response.statusCode);
+                     */
+
+                    if (response.statusCode === 404) {
+                        resolve({
+                            isValid: false,
+                            url: guessUrl,
+                            reason: '404 Not found.'
+                        });
+                        return;
+                    }
+                }
+
+                if (typeof body === 'string' && !body.match(/<html>/)) {
+                    /*
+                     console.log('body #' + i, body);
+                     */
+                    resolve({
+                        isValid: true,
+                        url: guessUrl,
+                        actualUrl: response.request.href
+                    });
+
+                    for (var k = 0; k < crawlers.length; k++) {
+                        if (k !== i) {
+                            crawlers[k].abort();
+                        }
+                    }
+                    return;
+                }
+                reject('Crawler for ' + guessUrl + ' reached an unknown state');
+            })
+            .on('abort', function _onAbort() {
+                /*
+                 console.log('Aborted #' + i + ':', guessUrl);
+                 */
+                resolve({
+                    isValid: false,
+                    url: guessUrl,
+                    reason: 'Aborted #' + i + ':'
+                }, guessUrl);
+            });
+
+        crawlers.push(crawler);
+    });
+};
+
+function _guessLicenseUrl(urlBase) {
+    var promises = [];
+    var crawlers = [];
+
+    for (var i = 0; i < _licenseGuesses.length; i++) {
+        const guessUrl = urlBase + _licenseGuesses[i];
+
+        promises.push(createHypothesisCrawler(guessUrl, i, crawlers));
+    }
+
+    return Promise.all(promises)
+        .then(function _filterLicenses(results) {
+            return Promise.resolve(_.filter(results, ['isValid', true]));
+        });
+}
+
+function getLicenseText(url) {
+    return new Promise(function (resolve, reject) {
+        request(url, function _returnLicenseText(error, response, body) {
+            if (!error && typeof body === 'string' && !body.match(/<html>/)) {
+                resolve(body);
+            } else {
+                resolve(null);
             }
-        }, []);
+        });
+    });
+}
 
-    var now = new Date();
+var _licenceRegEx = /license\.?(txt|md)?$/i;
 
-    var args = program.rawArgs.splice(2);
+function addLicenseText(dependency) {
+    return new Promise(function (resolve, reject) {
+        if (!dependency.licenseUrl.match(_licenceRegEx)) {
+            if (dependency.licenseUrl === dependency.repository) {
+                _guessLicenseUrl(dependency.licenseUrl + '/raw/master/')
+                    .then(function _onFulfilled(results) {
+                        dependency.licenseUrl = _.first(results).url;
 
-    return {
-        name: packageJson.name,
-        version: packageJson.version,
-        comment: 'Dependency information automatically extracted with "' +
-        program.name() + ' ' + args.join(' ') + '"',
-        generated: now.toLocaleString(),
-        dependencies: dependencies
-    };
+                        getLicenseText(dependency.licenseUrl).then(function _setLicenseText(value) {
+                            dependency.licenseText = value;
+                            resolve(value);
+                        });
+                    }, function _onRejected(results) {
+                        dependency.licenseUrl = null;
+                        resolve();
+                    });
+            } else {
+                consoleLog(chalk.red('Don\'t know how to handle the licenseUrl value'),
+                    chalk.cyan(dependency.licenseUrl));
+                resolve();
+            }
+        } else {
+            getLicenseText(dependency.licenseUrl).then(function _setLicenseText(value) {
+                dependency.licenseText = value;
+                resolve(value);
+            });
+        }
+    });
+}
+
+function parseCrawlerResults(packageJson, licences) {
+    return new Promise(function (resolve, reject) {
+        var dependencies = _.transform(licences,
+            function _dependencyTransformer(result, value, key) {
+                var parts = key.split('@'),
+                    version = parts.pop(),
+                    name = parts.join('@');
+
+                if (!program.strict || isPackageDependency(name, value, packageJson)) {
+                    result.push({
+                        name: name,
+                        version: version,
+                        licenses: value.licenses,
+                        repository: _cleanupUrls(value.repository),
+                        licenseUrl: _cleanupUrls(value.licenseUrl),
+                        parents: value.parents
+                    });
+                }
+            }, []);
+
+        Promise.all(_.map(dependencies, addLicenseText))
+            .then(function onResolve(promises) {
+                var now = new Date();
+
+                var args = program.rawArgs.splice(2);
+
+                resolve({
+                    name: packageJson.name,
+                    version: packageJson.version,
+                    comment: 'Dependency information automatically extracted with "' +
+                    program.name() + ' ' + args.join(' ') + '"',
+                    generated: now.toLocaleString(),
+                    dependencies: dependencies
+                });
+            }, function onRejected(reason) {
+                reject(reason);
+            });
+    });
 }
 
 function replaceUndefinedByNull(key, value) {
@@ -171,12 +325,16 @@ function parseDependencies() {
         gulp: false
     }, function (error, results) {
         console.log = origConsoleLog;  // Enable log.
-        var data = parseCrawlerResults(packageJson, results);
+        parseCrawlerResults(packageJson, results)
+            .then(function _onResolve(data) {
+                if (dependenciesExtra) {
+                    extendWithExtraInfo(data, dependenciesExtra, accumulatedExtra);
+                }
+                output(data);
+            }, function onRejected(reason) {
+                consoleLog(chalk.red(reason));
+            });
 
-        if (dependenciesExtra) {
-            extendWithExtraInfo(data, dependenciesExtra, accumulatedExtra);
-        }
-        output(data);
     });
 }
 
